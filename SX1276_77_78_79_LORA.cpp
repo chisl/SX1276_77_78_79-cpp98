998da6474b4e1091063018e4e09a9a431da733a9
chisl <info@chisl.io>
2017-12-27 18:15:55 +0100

    Initial commit

---
+++ SX1276_77_78_79_LORA.cpp
@@ -0,0 +1,13 @@
+/*
+ * name:        SX1276_77_78_79
+ * description: 137 MHz to 1020 MHz Low Power Long Range Transceiver featuring the LoRa (TM) long range modem
+ * manuf:       Semtech
+ * version:     0.1
+ * url:         http://www.semtech.com/images/datasheet/sx1276_77_78_79.pdf
+ * date:        2016-08-01
+ * author       https://chisl.io/
+ * file:        SX1276_77_78_79_LORA.cpp
+ */
+
+#include "SX1276_77_78_79_LORA.hpp"
+
